044293f18af3938f21c302a4373991c1ec4b10d1
Marco Luthi <marcopololuthi@gmail.com>
2019-10-23 19:54:18 +0200

    Removed Keys

--- src/main.js
+++ src/main.js
@@ -11,12 +11,6 @@ Vue.use(VueFirestore);
 
 firebase.initializeApp({
   //add your api keys
-  apiKey: "prototype-da432.firebaseapp.com",
-  authDomain: "prototype-da432.firebaseapp.com",
-  databaseURL: "prototype-da432.firebaseio.com",
-  projectId: "prototype-da432",
-  storageBucket: "prototype-da432.appspot.com",
-  messagingSenderId: "593384736192"
 });
 
 
